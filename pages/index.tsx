3affb30e148a5c8e1d9be0acdfe9214a84f07bc3
unknown <palo.loui.andrae@iisgalvanimi.edu.it>
2022-03-13 20:03:13 +0100

    (feat) chakra ui

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,46 @@
-import type { NextPage } from 'next'
+import type { NextPage } from "next";
 import { sendMail } from "../services/sendMail";
 
+import { Input, Textarea, Button, VStack, Box, Flex, Text } from "@chakra-ui/react";
+
 const Home: NextPage = () => {
-  const buttonWrapperStyle = {
-    color: "white",
-    display: "inline-block",
-    backgroundColor: "DodgerBlue",
-    padding: "10px",
-    fontFamily: "Arial",
-    maxWidth: '240px',
-    margin: '0 auto',
-    borderRadius: '3px'
-  }
+    return (
+        <Flex width='full' align='center' justifyContent='center'>
+            <Box p={2}>
+                <Box p={8} maxWidth='500px' borderWidth={1} borderRadius={8} boxShadow='lg'>
+                    <Box textAlign='center' mb={2}>
+                        <Text fontSize='lg'>NextJS + SendinBlue + ChakraUi</Text>
+                    </Box>
+                    <form onSubmit={sendMail}>
+                        <VStack>
+                            <Input id='name' name='name' placeholder='Name' size='md' maxWidth={96} isRequired />
+                            <Input
+                                id='subject'
+                                name='subject'
+                                placeholder='Subject'
+                                size='md'
+                                maxWidth={96}
+                                isRequired
+                            />
+                            <Input id='email' name='email' placeholder='Email' size='md' maxWidth={96} isRequired />
+                            <Textarea
+                                id='msg'
+                                name='message'
+                                placeholder='Here is a sample placeholder'
+                                size='md'
+                                maxWidth={96}
+                                resize='none'
+                                isRequired
+                            />
+                            <Button as='button' type='submit' width={72} maxWidth={96}>
+                                Send
+                            </Button>
+                        </VStack>
+                    </form>
+                </Box>
+            </Box>
+        </Flex>
+    );
+};
 
-  return (
-    <form style={buttonWrapperStyle} onSubmit={sendMail}>
-      <input id="name" name="name" type="text" required />
-      <input id="subject" name="subject" type="text" required />
-      <input id="email" name="email" type="text" required />
-      <input id="msg" name="message" type="text" required />
-      <button type='submit'>Send</button>
-    </form>
-  );
-}
-
-export default Home
+export default Home;
